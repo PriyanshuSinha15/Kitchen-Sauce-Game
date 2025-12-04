dbf10a8f40ba34cb090afdd880eb3aa316753f9d
Priyanshu Sinha <priyanshusinharockmans@gmail.com>
2025-12-05 00:33:27 +0530

    Plates Grabbed Event Done

--- Assets/Scripts/Counters/PlatesCounter.cs
+++ Assets/Scripts/Counters/PlatesCounter.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class PlatesCounter : BaseCounter
 {
     public event EventHandler OnPlateSpawned;
+    public event EventHandler OnPlateRemoved;
+
     [SerializeField] KitchenObjectSO plateKitchenObjectSO;
 
     private float spawnPlateTimer;
@@ -26,6 +28,23 @@ public class PlatesCounter : BaseCounter
                 platesSpawnedAmount++;
 
                 OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public override void Interact(Player player)
+    {
+        if (!player.HasKitchenObject())
+        {
+            //Player is empty handed
+            if(platesSpawnedAmount > 0)
+            {
+                // There is atleast one plate here
+                platesSpawnedAmount--;
+
+                KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, player);
+
+                OnPlateRemoved?.Invoke(this, EventArgs.Empty);
             }
         }
     }
--- Assets/Scripts/Counters/PlatesCounterVisual.cs
+++ Assets/Scripts/Counters/PlatesCounterVisual.cs
@@ -17,7 +17,15 @@ public class PlatesCounterVisual : MonoBehaviour
 
     void Start()
     {
-        platesCounter.OnPlateSpawned += PlatesCounter_OnPlateSpawned;   
+        platesCounter.OnPlateSpawned += PlatesCounter_OnPlateSpawned;
+        platesCounter.OnPlateRemoved += PlatesCounter_OnPlateRemoved;
+    }
+
+    private void PlatesCounter_OnPlateRemoved(object sender, System.EventArgs e)
+    {
+        GameObject plateGameObject = platesVisualGameObjectList[platesVisualGameObjectList.Count - 1];
+        platesVisualGameObjectList.Remove(plateGameObject);
+        Destroy(plateGameObject);
     }
 
     private void PlatesCounter_OnPlateSpawned(object sender, System.EventArgs e)
